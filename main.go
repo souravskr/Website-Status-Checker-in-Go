15a481edfd3c17547dfa6ac76c747043c2f15e0d
Sourav Sarker <souravsker@gmail.com>
2022-12-30 18:03:48 -0330

    Go routines and channels to check websites status

--- main.go
+++ main.go
@@ -15,16 +15,24 @@ func main() {
 		"https://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, website := range websites {
-		checkWebsite(website)
+		go checkWebsite(website, c)
+	}
+
+	for i := 0; i < len(websites); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkWebsite(site string) {
+func checkWebsite(site string, c chan string) {
 	res, err := http.Get(site)
 	if err != nil {
 		fmt.Println("Error:", err)
+		c <- "The site might be down!"
 		os.Exit(1)
 	}
 	fmt.Println(site, "status is", res.Status)
+	c <- "The website is up!"
 }
