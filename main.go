1e5457ec13260bebecd3349f046b28fb9573d1ee
Sourav Sarker <souravsker@gmail.com>
2022-12-30 14:54:39 -0330

    Check Status of The website - Without Concurrency

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+func main() {
+	websites := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://golang.org",
+		"https://amazon.com",
+	}
+
+	for _, website := range websites {
+		checkWebsite(website)
+	}
+}
+
+func checkWebsite(site string) {
+	res, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	fmt.Println(site, "status is", res.Status)
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Status Checker for some common websites that exists online.
+
+##### The program takes a list of some popular websites and makes `http get` requests to each of those sites to check whether these websites are up and responding to http traffic.
